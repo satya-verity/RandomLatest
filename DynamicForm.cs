0fcb269de6a0d1853eab14b66f0e8b9288abd7c1
satya-verity <84212956+satya-verity@users.noreply.github.com>
2023-12-13 12:24:57 -0500

    Update DynamicForm.cs

--- DynamicForm.cs
+++ DynamicForm.cs
@@ -1,6 +1,9 @@
-﻿using ClientApi;
+using ClientApi;
+using Connex.DynamicForms.Controls;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Connex.DynamicForms
@@ -23,20 +26,28 @@ namespace Connex.DynamicForms
 
         public DialogResult ShowDialog(string formDefinition, IDictionary<string, object> formData, Action<string, object, DialogAction> callBacK)
         {
-            this.form = this.formHandler.ConstructForm(formDefinition);
-            this.form = this.formHandler.InitializeValues(form, formData, callBacK);
+            try
+            {
+                this.form = this.formHandler.ConstructForm(formDefinition);
+                this.form = this.formHandler.InitializeValues(form, formData, callBacK);
 
-            return this.formHandler.ShowFormAsDialog(form);          
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw ex;
+            }
+            return this.formHandler.ShowFormAsDialog(form);
         }
 
         public void DlgEnable(string controlName, bool enable)
         {
-            throw new NotImplementedException();
+            this.FindControl(controlName).Enabled = enable;
         }
 
         public bool DlgEnable(string controlName)
         {
-            throw new NotImplementedException();
+            return this.FindControl(controlName).Enabled;
         }
 
         public void DlgEnd(int exitCode)
@@ -46,62 +57,87 @@ namespace Connex.DynamicForms
 
         public bool DlgFocus(string controlName)
         {
-            throw new NotImplementedException();
+            return this.FindControl(controlName).ContainsFocus;
         }
 
         public void DlgListBoxArray(string controlName, List<string> entries)
         {
-            throw new NotImplementedException();
+            var control = this.FindControl(controlName);
+            IControlBinder<Control> controlBinder = ControlBinderFactory.CreateControlBinder(control.GetType().Name);
+            if (controlBinder is IListControl listControlBinder)
+            {
+                listControlBinder.SetListBoxArray(control, entries);
+            }
         }
 
         public List<string> DlgListBoxArray(string controlName)
         {
-            throw new NotImplementedException();
+            var control = this.FindControl(controlName);
+            IControlBinder<Control> controlBinder = ControlBinderFactory.CreateControlBinder(control.GetType().Name);
+            if (controlBinder is IListControl listControlBinder)
+            {
+                return listControlBinder.GetListBoxArray(control);
+            }
+            return null;
         }
 
         public void DlgText(string controlName, string text)
         {
-            throw new NotImplementedException();
+            var control = this.FindControl(controlName);
+            IControlBinder<Control> controlBinder = ControlBinderFactory.CreateControlBinder(control.GetType().Name);
+            controlBinder.SetControlText(control, text);
         }
 
         public string DlgText(string controlName)
         {
-            throw new NotImplementedException();
-        }
-
-        public void DlgValue(string controlName, object value)
-        {
-            throw new NotImplementedException();
+            var control = this.FindControl(controlName);
+            IControlBinder<Control> controlBinder = ControlBinderFactory.CreateControlBinder(control.GetType().Name);
+            return controlBinder.GetControlText(control);
         }
 
         public void DlgValue(string controlName, int value)
         {
-            throw new NotImplementedException();
+            var control = this.FindControl(controlName);
+            IControlBinder<Control> controlBinder = ControlBinderFactory.CreateControlBinder(control.GetType().Name);
+            controlBinder.SetControlValueFromInt(control, value);
         }
 
-        public string DlgValue(string controlName)
+        public int DlgValue(string controlName)
         {
-            throw new NotImplementedException();
+            var control = this.FindControl(controlName);
+            IControlBinder<Control> controlBinder = ControlBinderFactory.CreateControlBinder(control.GetType().Name);
+            return controlBinder.GetControlValueAsInt(control);
         }
 
         public void DlgVisible(string controlName, bool visible)
         {
-            throw new NotImplementedException();
+            this.FindControl(controlName).Visible = visible;
         }
 
         public bool DlgVisible(string controlName)
         {
-            throw new NotImplementedException();
+            return this.FindControl(controlName).Visible;
         }
 
         public void SetDlgFocus(string controlName)
         {
-            throw new NotImplementedException();
+            this.FindControl(controlName).Focus();
         }
 
         public void SetDlgVisible(string controlName, bool visible)
         {
-            throw new NotImplementedException();
+            this.FindControl(controlName).Visible = visible;
+        }
+
+        private Control FindControl(string controlName)
+        {
+            var control = this.form.Controls[controlName];
+            if (control == null)
+            {
+                var error = $"There is no control with the name {controlName}.";
+                Debug.WriteLine(error);
+            }
+            return control;
         }
     }
 }
